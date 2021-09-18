5476e941ebd0da2512571af8b8e71c5888673cc9
KulshresthJangid <71918448+KulshresthJangid@users.noreply.github.com>
2021-09-18 23:18:09 +0530

    The first ever commit

---
+++ js/client-js.js
@@ -0,0 +1,4 @@
+// ScrollReveal().reveal('.card-1', { delay: 500 })
+// ScrollReveal().reveal('.footer', { delay: 500 })
+// ScrollReveal().reveal('.carousel', { delay: 500 })
+// ScrollReveal().reveal('.clients', { delay: 500 })
