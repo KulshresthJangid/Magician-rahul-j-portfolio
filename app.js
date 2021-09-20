9a5b1aedb905d5bdfbb4beb1a718a5a7914f739f
KulshresthJangid <71918448+KulshresthJangid@users.noreply.github.com>
2021-09-21 00:24:58 +0530

    Backend implemented

---
+++ app.js
@@ -0,0 +1,30 @@
+const { application } = require('express')
+const express = require('express')
+const app = express()
+const path = require('path')
+const { nextTick } = require('process')
+
+const publicDirectoryPath = path.join(__dirname, '/public')
+
+app.use(express.static(publicDirectoryPath))
+
+console.log(publicDirectoryPath)
+
+app.get('/', (req, res) => {
+    var options = {
+        root: path.join(__dirname)
+    }
+
+    var fileName = 'index.html'
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            next(err)
+        } else {
+            console.log('Sent:', fileName);
+        }
+    })
+})
+
+app.listen(process.env.PORT || 3000 , (req, res) => {
+    console.log(`Server is up and running on 3000`)
+})
